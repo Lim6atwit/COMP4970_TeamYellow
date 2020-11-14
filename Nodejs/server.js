5f672f624267816e326fa6316f35552101141059
kashemaatwit <65506977+kashemaatwit@users.noreply.github.com>
2020-11-14 18:25:12 -0500

    Update server.js

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const roomRouter = require("./api/room/room.router");
+const speakerRouter = require("./api/speaker/speaker.router");
+const timeslotRouter = require("./api/timeslot/timeslot.router");
+
 
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
 app.use("/api/room", roomRouter);
-const port = process.env.PORT || 4000;;
+app.use("/api/speaker", speakerRouter);
+app.use("/api/timeslot", timeslotRouter);
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log("server up and running on PORT :", port);
 });
