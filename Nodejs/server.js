859b19eab8da65fb44439fc860587b778d061aa2
kashemaatwit <65506977+kashemaatwit@users.noreply.github.com>
2020-11-14 18:33:01 -0500

    Update server.js

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const app = express();
 const roomRouter = require("./api/room/room.router");
 const speakerRouter = require("./api/speaker/speaker.router");
@@ -11,7 +10,7 @@ const timeslotRouter = require("./api/timeslot/timeslot.router");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
+
 
 app.use("/api/room", roomRouter);
 app.use("/api/speaker", speakerRouter);
