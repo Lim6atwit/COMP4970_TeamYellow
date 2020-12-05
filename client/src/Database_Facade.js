da2fbcca06a1bd15ece73f16f0f387176e6e2ddf
James Kerr <kerrj@wit.edu>
2020-12-04 19:51:32 -0500

    reimplement verify

--- client/src/Database_Facade.js
+++ client/src/Database_Facade.js
@@ -2,11 +2,121 @@ import axios from 'axios';
 
 class Database_Facade {
     
-    verifySession(session) { return true}
-    verifyTimeslot(timeslot) { return true}
-    verifyRoom(room) { return true}
-    verifySpeaker(speaker) { return true}
     
+     verifyTimeslot(timeslot) {
+        function verifyTimeFormat(time){
+            var timeFormat = /[0-2][0-9]:[0-5][0-9]/;
+            if (time.match(timeFormat)) {
+                alert("Please enter using alphanumeric characters only");
+                return (false);
+            }
+            else {
+                return (true);
+            }
+        }
+        function verifyTimeOverlap(startTime,endTime){
+            var startHour = Number(startTime.substr(0,2));
+            var startMinute = Number(startTime.substr(3,2));
+            
+            var endHour = Number(endTime.substr(0,2));
+            var endMinute = Number(endTime.substr(3,2));
+
+            if(endHour<startHour){
+                alert("Start time is after end time");
+                return false;
+            }else if(endHour==startHour){
+                if(endMinute<startMinute){
+                    alert("Start time is after end time");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return (verifyTimeFormat(timeslot.startTime) && verifyTimeFormat(timeslot.endTime) && verifyTimeOverlap(timeslot.startTime, timeslot.endTime));
+    }
+    
+
+    verifyRoom(room) {
+        function verifySeatCount(seatCount) {
+            if (seatCount > 0) {
+                return (true);
+            }
+            else {
+                alert("seatcount cannot be <=0")
+                return (false);
+            }
+        }
+        function verifyRoomName(roomName) {
+            var illegalCharacters = /[\(\)\{\}\[\]<>?;:,?\/*+:!@#$%^_="|\\~`]/;
+            if (roomName.match(illegalCharacters)) {
+                alert("Please enter using alphanumeric characters only");
+                return (false);
+            }
+            else {
+                return (true);
+            }
+        }
+
+        return (verifySeatCount(room.seatCount) && verifyRoomName(room.roomName))
+    }
+
+    verifySpeaker(speaker) {
+        function verifyPhone(number) {
+            var phoneformat = /^\+\d{11}$/;
+            var builtPhoneNumber="";
+            for(var i=0;i<number.length;i++){
+                if(number.substr(i,1).match(/\d/)){
+                    builtPhoneNumber+=number.substr(i,1);
+                }
+            }
+            if(builtPhoneNumber.length==10){builtPhoneNumber=`+1${builtPhoneNumber}`;}
+
+            if (builtPhoneNumber.value.match(phoneformat)) {
+                return (true);
+
+            }
+            else {
+                alert("Please enter numerical characters only");
+                return (false);
+            }
+        }
+
+        function verifyName(name) {
+            var illegalCharacters = /[(){}[]<>?;:,?\/*+!@#$%^&_="|\\~`]+/;
+            if (name.value.match(illegalCharacters)) {
+                alert("Please input alphanumeric characters only");
+                return (false);
+            }
+            else {
+                return (true);
+            }
+
+        }
+
+        function verifyEmail(email) {
+            var emailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(String(email).toLowerCase());
+            if (email.value.match(emailformat)) {
+                return (true);
+
+            }
+            else {
+                alert("You have entered an invalid email address!");
+                return (false);
+            }
+        }
+
+        return (verifyPhone(speaker.cellPhone) && verifyPhone(speaker.dayPhone) && verifyName(speaker.speakerName) && verifyEmail(speaker.email))
+    }
+
+    verifySession(session) {
+        //room_id, speaker_id, timeslot_id
+        var room = this.queryRoom(session.room_id);
+        var timeslot = this.queryTimeslot(session.room_id);
+        var speaker = this.querySpeaker(session.room_id);
+        
+        return (this.verifyTimeslot(timeslot) && this.verifyRoom(room) && this.verifySpeaker(speaker));
+    }
 
 
     async insertSession(session) {
@@ -93,9 +203,6 @@ class Database_Facade {
         return response;
             
     }
-    
-    
-    
 
     async querySessions() {
 
@@ -194,119 +301,3 @@ class Database_Facade {
 }
 
 export default Database_Facade;
-
-/*
- verifyTimeslot(timeslot) {
-        function verifyTimeFormat(time){
-            var timeFormat = /[0-2][0-9]:[0-5][0-9]/;
-            if (time.match(timeFormat)) {
-                alert("Please enter using alphanumeric characters only");
-                return (false);
-            }
-            else {
-                return (true);
-            }
-        }
-        function verifyTimeOverlap(startTime,endTime){
-            var startHour = Number(startTime.substr(0,2));
-            var startMinute = Number(startTime.substr(3,2));
-            
-            var endHour = Number(endTime.substr(0,2));
-            var endMinute = Number(endTime.substr(3,2));
-
-            if(endHour<startHour){
-                alert("Start time is after end time");
-                return false;
-            }else if(endHour==startHour){
-                if(endMinute<startMinute){
-                    alert("Start time is after end time");
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        return (verifyTimeFormat(timeslot.startTime) && verifyTimeFormat(timeslot.endTime) && verifyTimeOverlap(timeslot.startTime, timeslot.endTime));
-    }
-
-    verifyRoom(room) {
-        function verifySeatCount(seatCount) {
-            if (seatCount > 0) {
-                return (true);
-            }
-            else {
-                alert("seatcount cannot be <=0")
-                return (false);
-            }
-        }
-        function verifyRoomName(roomName) {
-            var illegalCharacters = /[\(\)\{\}\[\]<>?;:,?\/*+:!@#$%^_="|\\~`]/;
-            if (roomName.match(illegalCharacters)) {
-                alert("Please enter using alphanumeric characters only");
-                return (false);
-            }
-            else {
-                return (true);
-            }
-        }
-
-        return (verifySeatCount(room.seatCount) && verifyRoomName(room.roomName))
-    }
-
-    verifySpeaker(speaker) {
-        function verifyPhone(number) {
-            var phoneformat = /^\+\d{11}$/;
-            var builtPhoneNumber="";
-            for(var i=0;i<number.length;i++){
-                if(number.substr(i,1).match(/\d/)){
-                    builtPhoneNumber+=number.substr(i,1);
-                }
-            }
-            if(builtPhoneNumber.length==10){builtPhoneNumber=`+1${builtPhoneNumber}`;}
-
-            if (builtPhoneNumber.value.match(phoneformat)) {
-                return (true);
-
-            }
-            else {
-                alert("Please enter numerical characters only");
-                return (false);
-            }
-        }
-
-        function verifyName(name) {
-            var illegalCharacters = /[(){}[]<>?;:,?\/*+!@#$%^&_="|\\~`]+/;
-            if (name.value.match(illegalCharacters)) {
-                alert("Please input alphanumeric characters only");
-                return (false);
-            }
-            else {
-                return (true);
-            }
-
-        }
-
-        function verifyEmail(email) {
-            var emailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(String(email).toLowerCase());
-            if (email.value.match(emailformat)) {
-                return (true);
-
-            }
-            else {
-                alert("You have entered an invalid email address!");
-                return (false);
-            }
-        }
-
-        return (verifyPhone(speaker.cellPhone) && verifyPhone(speaker.dayPhone) && verifyName(speaker.speakerName) && verifyEmail(speaker.email))
-    }
-
-    verifySession(session) {
-        //room_id, speaker_id, timeslot_id
-        var room = this.queryRoom(session.room_id);
-        var timeslot = this.queryTimeslot(session.room_id);
-        var speaker = this.querySpeaker(session.room_id);
-        
-        return (this.verifyTimeslot(timeslot) && this.verifyRoom(room) && this.verifySpeaker(speaker));
-    }
-*/
